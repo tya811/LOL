78a79d4fafd4a888e76281419f9a0b22580ba640
tya811 <35475173+tya811@users.noreply.github.com>
2018-01-19 13:09:11 -0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,8 @@
+x=[[0 for z in range(10)]for y in range(5)]
+for line in(x):
+		print ("This is the idiotbox:", line)
+print ("I need examples on how to read, use and write documentation")
+print ("preferably in assembly")
+print ("I way hacked out a recording studio in my room but its practically a joke how unsupported programing is")
+print ("How oh how do I read make files") 
+print ("I cannot wait to see what replies I get for this one")
